03928524e9367d815f5e408043f2b89f921636eb
Mila <nocturnetheater@gmail.com>
2023-09-03 18:16:07 +0200

    add: first exercise

---
+++ Modulo 3 - Typescript/Ejercicio1.ts
@@ -0,0 +1,31 @@
+/* Un texto en formato CSV tiene el nombre de los campos en la primera fila y los datos en el resto, separados por comas. Crea un parseador que reciba un string en formato CSV y devuelva una colección de objetos. Utiliza destructuring, rest y spread operator donde creas conveniente. */
+
+const data = `id,name,surname,gender,email,picture
+15519533,Raul,Flores,male,raul.flores@example.com,https://randomuser.me/api/portraits/men/42.jpg
+82739790,Alvaro,Alvarez,male,alvaro.alvarez@example.com,https://randomuser.me/api/portraits/men/48.jpg
+37206344,Adrian,Pastor,male,adrian.pastor@example.com,https://randomuser.me/api/portraits/men/86.jpg
+58054375,Fatima,Guerrero,female,fatima.guerrero@example.com,https://randomuser.me/api/portraits/women/74.jpg
+35133706,Raul,Ruiz,male,raul.ruiz@example.com,https://randomuser.me/api/portraits/men/78.jpg
+79300902,Nerea,Santos,female,nerea.santos@example.com,https://randomuser.me/api/portraits/women/61.jpg
+89802965,Andres,Sanchez,male,andres.sanchez@example.com,https://randomuser.me/api/portraits/men/34.jpg
+62431141,Lorenzo,Gomez,male,lorenzo.gomez@example.com,https://randomuser.me/api/portraits/men/81.jpg
+05298880,Marco,Campos,male,marco.campos@example.com,https://randomuser.me/api/portraits/men/67.jpg
+61539018,Marco,Calvo,male,marco.calvo@example.com,https://randomuser.me/api/portraits/men/86.jpg`;
+
+const fromCSV = (csv: string) => {
+  const lines = csv.split("\n");
+  const headers = lines[0].split(",");
+  let result: any[] = [];
+  for (let i = 1; i < lines.length; i++) {
+    const currentLineArr = lines[i].split(",");
+    let obj = {};
+    for (let j = 0; j < currentLineArr.length; j++) {
+      obj = { ...obj, [headers[j]]: currentLineArr[j] };
+    }
+    result = [...result, obj];
+  }
+  return result;
+};
+
+const result = fromCSV(data);
+console.log(result);
